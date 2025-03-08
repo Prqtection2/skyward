163d5a897fcf80c844a2e29a45238ed11f6ba49f
Neev Kumar <neevkumar09@gmail.com>
2025-03-07 23:25:00 -0600

    chromerdriver 10

--- utils/skyward.py
+++ utils/skyward.py
@@ -31,14 +31,14 @@ class SkywardGPA:
     def calculate(self):
         try:
             options = webdriver.ChromeOptions()
-            options.add_argument('--headless')
+            options.add_argument('--headless=new')  # new headless mode
             options.add_argument('--no-sandbox')
             options.add_argument('--disable-dev-shm-usage')
             options.add_argument('--disable-gpu')
-            options.add_argument('--disable-software-rasterizer')
+            options.add_argument('--remote-debugging-port=9222')
             
-            service = Service('/usr/local/bin/chromedriver')
-            self.driver = webdriver.Chrome(service=service, options=options)
+            # Let Selenium 4.15+ handle driver management
+            self.driver = webdriver.Chrome(options=options)
             
             self.login()
             self.navigate_to_gradebook()

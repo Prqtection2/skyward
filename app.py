c89ce4ec73a3637a8060d7b4d53bc51bbf44d9ce
Neev Kumar <neevkumar09@gmail.com>
2025-03-14 18:00:00 -0500

    added to port 80

--- app.py
+++ app.py
@@ -26,5 +26,5 @@ def calculate():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 80))
     app.run(host='0.0.0.0', port=port, debug=False)
